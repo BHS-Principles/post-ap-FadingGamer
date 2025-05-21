b703a92b7566a89fbc742ea32302fecf36eb16b3
FadingGamer <117684973+FadingGamer@users.noreply.github.com>
2025-05-21 16:37:29 +0000

    Feeding my family

--- js.js
+++ js.js
@@ -1,8 +1,14 @@
-var LIST = [
-    "Frozen burritoes",
-    "Banna foster",
-    "Egg mcMuffin with egg"
-];
-for(let i = 0; i < 3; i++){
-alert("Frozen burritoes");
+var BTN = document.getElementById("myButton");
+
+var doSomething = function(event){
+    alert("Praise me!");
+    console.log(event);
 };
+
+for(var i = 0; i < 3; i++){
+    var newBTN = BTN.cloneNode(true);
+    document.body.append(newBTN);
+    newBTN.addEventListener("click", doSomething);
+};
+
+alert("PLEASE LET IT WORK!!!!!");
